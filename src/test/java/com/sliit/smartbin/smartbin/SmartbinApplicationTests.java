67e18a7327b82c0f59bc6b716e9a2cf5f30c3925
Sandaru Peiris <sandaru3n@gmail.com>
2025-10-14 16:40:17 +0530

    Initial commit

---
+++ src/test/java/com/sliit/smartbin/smartbin/SmartbinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.smartbin.smartbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
